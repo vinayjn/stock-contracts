3c110c517e0ed8fc3955aec1075a40027f932524
Vinay Jain <vinay.jn7@gmail.com>
2019-06-13 15:57:58 +0530

    Added new files

---
+++ Contracts/Contracts.h
@@ -0,0 +1,20 @@
+//
+//  Contracts.h
+//  Contracts
+//
+//  Created by Vinay Jain on 11/06/19.
+//  Copyright © 2019 Vinay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Contracts.
+FOUNDATION_EXPORT double ContractsVersionNumber;
+
+//! Project version string for Contracts.
+FOUNDATION_EXPORT const unsigned char ContractsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Contracts/PublicHeader.h>
+
+
+#import Contracts-Swift.h
