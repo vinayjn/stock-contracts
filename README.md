7dffe351b7dda088010c901c443b121e0037e38d
Vinay Jain <vinay.jn7@gmail.com>
2021-04-25 22:21:19 +0530

    Moved to Swift Package

---
+++ README.md
@@ -0,0 +1,3 @@
+# StockContracts
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import StockContractsTests
+
+var tests = [XCTestCaseEntry]()
+tests += StockContractsTests.allTests()
+XCTMain(tests)
